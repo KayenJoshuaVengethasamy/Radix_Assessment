d3e862a31aa62a9e3f1a5a424191dd7ff0bb41a7
Kayen Joshua Vengethasamy <60103904+KayenJoshuaVengethasamy@users.noreply.github.com>
2026-06-19 09:46:18 +0200

    Starter Spring project
    Initial Spring project

---
+++ assessment/src/test/java/com/radix/assessment/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.radix.assessment;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
